685fb0b04f405847537a33d85705fbf4a66da884
Brian Parker <brian.parker@hotmail.com.au>
2021-05-27 16:35:13 +1000

    Initial restructure

--- ThemingIdeas/Program.cs
+++ ThemingIdeas/Program.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace ThemingIdeas
 {
@@ -14,7 +10,7 @@ namespace ThemingIdeas
     {
         public static async Task Main(string[] args)
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
